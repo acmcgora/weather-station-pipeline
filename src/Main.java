6a1929c77f837f8d5827a568c1b1f7f596e136d4
Ashton McGoran <acmcgora@mtu.edu>
2026-02-25 20:10:24 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import sensor.Sensor;
-import sampler.Sampler;
-import transformer.Transformer;
 import api.RestAPI;
 import database.Database;
+import sampler.Sampler;
+import sensor.Sensor;
+import transformer.Transformer;
 
 public class Main {
 
@@ -16,19 +16,31 @@ public class Main {
 
         System.out.println("Weather Station Pipeline Started...");
 
+        // Check for CI environment variable
+        String ciCyclesEnv = System.getenv("CI_CYCLES");
+        int maxCycles = ciCyclesEnv != null ? Integer.parseInt(ciCyclesEnv) : -1;
+
+        int cycles = 0;
+
+        // Use shorter sleep for CI
+        int sleepTime = (maxCycles > 0) ? 200 : 1000; // 200ms per iteration in CI, 1s locally
+
+        System.out.println("CI_CYCLES=" + ciCyclesEnv + "  maxCycles=" + maxCycles);
+
         while (true) {
             double voltage = sensor.generateVoltage();
-            System.out.println("Sensor voltage: " + voltage + " V");
-
             double sampled = sampler.sample(voltage);
-
             double temperature = transformer.voltageToTemperature(sampled);
-
             api.send(temperature);
-
             database.save(temperature);
 
-            Thread.sleep(1000);
+            Thread.sleep(sleepTime);
+
+            cycles++;
+            if (maxCycles > 0 && cycles >= maxCycles) {
+                System.out.println("Max cycles reached. Exiting...");
+                break;
+            }
         }
     }
 }
