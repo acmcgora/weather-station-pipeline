287808566a1ee0e9e6bb944a590dc664adbf43c3
Ashton McGoran <acmcgora@mtu.edu>
2026-03-19 17:50:33 -0400

    Fix import path for transformer tests

--- tests/transformer.test.py
+++ tests/transformer.test.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/transformer')))
+
+from Flaskapp import app
 import pytest
-from src.transformer.Flaskapp import app
 
 @pytest.fixture
 def client():
